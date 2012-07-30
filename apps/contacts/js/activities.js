22287d5f261c8a6c68fd134fa048eca99653514c
Jose M. Cantera <jmcanterafonseca@gmail.com>
2012-07-30 18:10:15 +0200

    First version with contacts integrated

---
+++ apps/contacts/js/activities.js
@@ -0,0 +1,60 @@
+var ActivityHandler = {
+  _currentActivity: null,
+
+  get currentlyHandling() {
+    return !!this._currentActivity;
+  },
+
+  get activityName() {
+    if (!this._currentActivity) {
+      return null;
+    }
+
+    return this._currentActivity.source.name;
+  },
+
+  handle: function ah_handle(activity) {
+
+    window.console.log('Activity Handler invoked');
+
+    this._currentActivity = activity;
+
+    switch (this.activityName) {
+      case 'new':
+        if(document.location !== 'contacts.html') {
+          document.location = 'contacts.html';
+          document.location.hash = 'view-contact-form';
+        }
+        if (this._currentActivity.source.data.params) {
+          var param, params = [];
+          for (var i in this._currentActivity.source.data.params) {
+            param = this._currentActivity.source.data.params[i];
+            params.push(i + '=' + param);
+          }
+          document.location.hash += '?' + params.join('&');
+        }
+        break;
+      case 'pick':
+        Contacts.navigation.home();
+        break;
+      case 'import':
+        document.location = 'fb_import.html';
+        break;
+    }
+  },
+
+  postNewSuccess: function ah_postNewSuccess(contact) {
+    this._currentActivity.postResult({contact: contact});
+    this._currentActivity = null;
+  },
+
+  postPickSuccess: function ah_postPickSuccess(number) {
+    this._currentActivity.postResult({ number: number });
+    this._currentActivity = null;
+  },
+
+  postCancel: function ah_postCancel() {
+    this._currentActivity.postError('canceled');
+    this._currentActivity = null;
+  }
+};
