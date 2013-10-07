b3b210431e80590863a4705cec9dcf6349265378
Jose M. Cantera <jmcanterafonseca@gmail.com>
2013-10-07 17:45:15 +0200

    WIP1

--- apps/communications/contacts/js/fb/fb_data.js
+++ apps/communications/contacts/js/fb/fb_data.js
@@ -3,173 +3,39 @@
 var fb = window.fb || {};
 
 if (!window.fb.contacts) {
-  (function(document) {
-
+  (function() {
     var contacts = fb.contacts = {};
-    var indexedDB = window.mozIndexedDB || window.webkitIndexedDB ||
-      window.indexedDB;
 
-    var database;
-    var DB_NAME = 'Gaia_Facebook_Friends';
-    var OLD_DB_VERSIONS = [1.0, 2.0];
-    var DB_VERSION = 3.0;
-    var OLD_STORE_NAMES = ['FBFriends', 'FBFriendsV2'];
-    var STORE_NAME = 'FBFriendsV3';
-    var INDEX_LONG_PHONE = 'byTelephone';
-    var INDEX_SHORT_PHONE = 'byShortTelephone';
+    var datastore;
+    var DATASTORE_NAME = 'Gaia_Facebook_Friends';
+    // Record Id for the index
+    var INDEX_ID = 1;
+
     var isInitialized = false;
-    var migrationNeeded = false;
-    var oldStoreName;
 
-    function DatabaseMigrator(items) {
-      var pointer = 0;
-      var CHUNK_SIZE = 5;
-      var numResponses = 0;
-      var self = this;
-
-      this.items = items;
-
-      function continueCb() {
-        numResponses++;
-        pointer++;
-        if (pointer < self.items.length && numResponses === CHUNK_SIZE) {
-          numResponses = 0;
-          migrateSlice(pointer);
-        }
-        else if (pointer >= self.items.length) {
-          if (typeof self.onfinish === 'function') {
-            self.onfinish();
-          }
-        }
-      }
-
-      this.start = function() {
-        if (Array.isArray(self.items) && self.items.length > 0) {
-          migrateSlice(0);
-        }
-        else {
-          if (typeof self.onfinish === 'function') {
-            self.onfinish();
-          }
-        }
-      };
-
-      function migrateSlice(from) {
-        for (var i = from; i < from + CHUNK_SIZE &&
-             i < self.items.length; i++) {
-          var req = new fb.utils.Request();
-          var item = self.items[i];
-          doSave(item, req);
-          req.onsuccess = function saveSuccess() {
-            console.log('FB Cache: Success migrating ', item.uid);
-            continueCb();
-          };
-          req.onerror = function saveError() {
-            console.error('FB Cache: Error migrating ', item.uid);
-            continueCb();
-          };
-        }
-      }
-    }
-
-    /**
-     *  Creates the store
-     *
-     */
-    function createStore(e) {
-      var db = e.target.result;
-      if (db.objectStoreNames.contains(STORE_NAME)) {
-        db.deleteObjectStore(STORE_NAME);
-      }
-      return db.createObjectStore(STORE_NAME, { keyPath: 'uid' });
-    }
-
-    function createStoreAndIndex(e) {
-      var store = createStore(e);
-      store.createIndex(INDEX_LONG_PHONE, 'telephone', {
-        unique: true,
-        multiEntry: true
-      });
-      store.createIndex(INDEX_SHORT_PHONE, 'shortTelephone', {
-        unique: true,
-        multiEntry: true
-      });
-    }
-
-    function upgradeDB(e) {
-      var oldDbSearch = OLD_DB_VERSIONS.indexOf(e.oldVersion);
-      if (oldDbSearch !== -1 && e.newVersion === DB_VERSION) {
-        window.console.warn('Upgrading Facebook Cache!!!!!');
-        migrationNeeded = true;
-        oldStoreName = OLD_STORE_NAMES[oldDbSearch];
-        createStoreAndIndex(e);
-      }
-      else if (e.newVersion === 1.0) {
-        createStore(e);
-      }
-      else if (e.newVersion === DB_VERSION) {
-        createStoreAndIndex(e);
-      }
-    }
-
-    function clearOldObjStore(cb) {
-      var transaction = database.transaction([oldStoreName], 'readwrite');
-      var objectStore = transaction.objectStore(oldStoreName);
-
-      var req = objectStore.clear();
-      req.onsuccess = cb;
-      req.onerror = function error_del_objstore(e) {
-        window.console.error('FB Cache. Error while clearing old Obj Store',
-                             e.target.error.name);
-        cb();
-      };
-    }
-
-    function migrateData(onfinishCb) {
-      if (!database.objectStoreNames.contains(oldStoreName)) {
-        onfinishCb();
-        return;
-      }
-
-      var transaction = database.transaction([oldStoreName], 'readonly');
-      var objectStore = transaction.objectStore(oldStoreName);
-
-      var req = objectStore.mozGetAll();
-
-      req.onsuccess = function(e) {
-        var data = e.target.result;
-
-        var migrator = new DatabaseMigrator(data);
-         migrator.onfinish = function migration_finished() {
-          window.console.log('FB Cache: Migration process finished!!');
-          migrationNeeded = false;
-          clearOldObjStore(onfinishCb);
-        };
-        migrator.start();
-      };
-
-      req.onerror = function(e) {
-        window.console.error('FB Cache: Data migration failed !!!! ');
-        onfinishCb();
-      };
-    }
+    // The index we need to keep
+    var index;
 
     function initError(outRequest, error) {
       outRequest.failed(error);
     }
 
     function doGet(uid, outRequest) {
-      var transaction = database.transaction([STORE_NAME], 'readonly');
-      var objectStore = transaction.objectStore(STORE_NAME);
-      var areq = objectStore.get(uid);
+      window.console.log('Index content', JSON.stringify(index));
 
-      areq.onsuccess = function(e) {
-        outRequest.done(e.target.result);
-      };
+      var dsId = index[uid];
 
-      areq.onerror = function(e) {
-        outRequest.failed(e.target.error);
-      };
+      window.console.log('DS Id: ', dsId);
+
+      datastore.get(dsId).then(function success(obj) {
+        window.console.log('Do Get: ', JSON.stringify(obj));
+        window.console.log(JSON.stringify(obj));
+        outRequest.done(obj);
+      },
+      function error(err) {
+        window.console.log('Error get: ', err);
+        outRequest.failed(err);
+      });
     }
 
     /**
@@ -234,29 +100,30 @@ if (!window.fb.contacts) {
     };
 
     function doSave(obj, outRequest) {
-      var transaction = database.transaction([STORE_NAME], 'readwrite');
-
-      transaction.onerror = function(e) {
-        outRequest.failed(e.target.error);
-      };
-
-      var objectStore = transaction.objectStore(STORE_NAME);
-
-      if (Array.isArray(obj.tel) && obj.tel.length > 0) {
-        obj.telephone = [];
-        obj.tel.forEach(function(atel) {
-          obj.telephone.push(atel.value);
+      var dsId = index[obj.uid];
+      if (typeof dsId === 'undefined') {
+        datastore.add(obj).then(function success(newId) {
+          window.console.log('Saved Id: ', newId, JSON.stringify(index));
+          try {
+             index[obj.uid] = newId;
+          }
+          catch(e) {
+            window.console.error(e);
+          }
+          outRequest.done(newId);
+        },
+        function error(err) {
+          outRequest.failed(err);
         });
       }
-
-      var req = objectStore.put(obj);
-      req.onsuccess = function(e) {
-        outRequest.done(e.target.result);
-      };
-
-      req.onerror = function(e) {
-        outRequest.failed(e.target.error);
-      };
+      else {
+        datastore.update(dsId, obj).then(function success() {
+          outRequest.done();
+        },
+        function error(err) {
+          outRequest.failed(err);
+        });
+      }
     }
 
     /**
@@ -279,32 +146,21 @@ if (!window.fb.contacts) {
       return retRequest;
     };
 
-    function doGetAll(outRequest) {
-      var transaction = database.transaction([STORE_NAME], 'readonly');
-      var objectStore = transaction.objectStore(STORE_NAME);
-
-      var req = objectStore.mozGetAll();
-
-      req.onsuccess = function(e) {
-        var data = e.target.result;
-        var out = {};
-        data.forEach(function(contact) {
-          out[contact.uid] = contact;
-        });
-        outRequest.done(out);
-      };
-
-      req.onerror = function(e) {
-        outRequest.failed(e.target.error);
-      };
+    function doGetLength(outRequest) {
+      datastore.getLength().then(function success (length) {
+        outRequest.done(length - 1);
+      },
+      function error(err) {
+        outRequest.failed(err);
+      });
     }
 
-    contacts.getAll = function() {
+    contacts.getLength = function() {
       var retRequest = new fb.utils.Request();
 
       window.setTimeout(function() {
         contacts.init(function get_all() {
-          doGetAll(retRequest);
+          doGetLength(retRequest);
         },
         function() {
           initError(retRequest);
@@ -315,17 +171,21 @@ if (!window.fb.contacts) {
     };
 
     function doRemove(uid, outRequest) {
-      var transaction = database.transaction([STORE_NAME], 'readwrite');
-      transaction.oncomplete = function(e) {
-        outRequest.done(e.target.result);
-      };
+      var dsId = index[uid];
 
-      transaction.onerror = function(e) {
-        outRequest.failed(e.target.error);
-      };
-      var objectStore = transaction.objectStore(STORE_NAME);
+      datastore.remove(dsId).then(function success(removed) {
+        if (removed) {
+          delete index[uid];
+          outRequest.done();
+        }
+        else {
+          outRequest.failed('Not removed');
+        }
 
-      objectStore.delete(uid);
+      },
+      function error(err) {
+        outRequest.failed(err);
+      });
     }
 
     /**
@@ -364,17 +224,19 @@ if (!window.fb.contacts) {
     };
 
     function doClear(outRequest) {
-      var transaction = database.transaction([STORE_NAME], 'readwrite');
-      transaction.oncomplete = function(e) {
-        outRequest.done(e.target.result);
-      };
-
-      transaction.onerror = function(e) {
-        outRequest.failed(e.target.error);
-      };
-      var objectStore = transaction.objectStore(STORE_NAME);
-
-      objectStore.clear();
+      datastore.clear().then(function success(cleared) {
+        if (cleared) {
+          index = {};
+          outRequest.done(cleared);
+          datastore.update(INDEX_ID, index);
+        }
+        else {
+          outRequest.failed('Not cleared');
+        }
+      },
+      function error(err) {
+        outRequest.failed(err);
+      });
     }
 
     function notifyOpenSuccess(cb) {
@@ -384,36 +246,66 @@ if (!window.fb.contacts) {
       }
     }
 
+    contacts.flush = function() {
+      var outRequest = new fb.utils.Request();
+
+      window.setTimeout(function do_Flush() {
+        if (!isInitialized) {
+          window.console.warn('The datastore has not been initialized');
+          outRequest.done();
+          return;
+        }
+        datastore.update(INDEX_ID, index).then(function success() {
+          outRequest.done();
+        }, function error(err) {
+          outRequest.failed();
+        });
+
+      }, 0);
+
+      return outRequest;
+    }
+
     contacts.init = function(cb, errorCb) {
       if (isInitialized === true) {
         cb();
         return;
       }
 
-      var req = indexedDB.open(DB_NAME, DB_VERSION);
-
-      req.onsuccess = function(e) {
-        database = e.target.result;
-        if (migrationNeeded === true) {
-          migrateData(function migrated() {
-            notifyOpenSuccess(cb);
-          });
+      navigator.getDataStores(DATASTORE_NAME).then(function success(ds) {
+        if(ds.length < 1) {
+          window.console.error('FB: Cannot get access to the DataStore');
+           if (typeof errorCb === 'function') {
+            errorCb();
+          }
+          return;
         }
-        else {
+
+        datastore = ds[0];
+        // Checking the length as the index should be there
+        datastore.getLength().then(function(length) {
+          if(length === 0) {
+            window.console.log('Adding index as length is 0');
+            index = {};
+            return datastore.add(index);
+          }
+          else {
+            return datastore.get(INDEX_ID);
+          }
+        }).then(function(v) {
+          window.console.log('Second promise resolved', JSON.stringify(v));
+          if(typeof v === 'object') {
+            index = Object.create(v);
+          }
           notifyOpenSuccess(cb);
-        }
-      };
-
-      req.onerror = function(e) {
-        window.console.error('FB: Error while opening the DB: ',
+        })
+      }, function error() {
+        window.console.error('FB: Error while opening the DataStore: ',
                                                         e.target.error.name);
         if (typeof errorCb === 'function') {
           errorCb();
         }
-      };
-
-      req.onupgradeneeded = upgradeDB;
+     });
     };
-
-  }) (document);
+  })();
 }
